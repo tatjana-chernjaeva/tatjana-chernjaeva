5a1f73bc1cc9a4771e2ee80488cac43690d366df
tatjana-chernjaeva <42931673+tatjana-chernjaeva@users.noreply.github.com>
2018-09-06 12:23:38 +0300

    Add files via upload

--- js/my_custom.js
+++ js/my_custom.js
@@ -1,14 +1,6 @@
 function send_msg() {
-	// alert("Пришли сюда 555");
-	//var frm;
-	//frm = document.getElementById("IFrame1");
-	//alert("Пришли сюда 555");
-	// alert(frm);
-	//var aaa = frm.contentWindow.document.getElementById("from_domen").value;  
-	//alert("Пришли сюда 555");
-	//alert(aaa);
+	// alert("Метка 5");
 	frm = document.getElementById("IFrame1");
-
 	frm.contentWindow.document.getElementById('sender_name').value = document.getElementById('send_name').value;
 	frm.contentWindow.document.getElementById('sender_email').value = document.getElementById('send_email').value;
 	frm.contentWindow.document.getElementById('sender_phone').value = document.getElementById('send_phone').value;;
@@ -17,6 +9,7 @@ function send_msg() {
 	frm.contentWindow.document.getElementById('from_domen').value = 'Tanjana-Chernyaeva';
 	frm.contentWindow.document.getElementById("send_btn").click();
 	
-	alert('Сообщение4 отправлено, спасибо!');
+	alert('Сообщение отправлено, спасибо!');
+	location.reload();
 		
 	}
