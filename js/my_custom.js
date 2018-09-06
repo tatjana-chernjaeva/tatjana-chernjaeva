a23d388284899629ab249927643d0c748e91e1ce
tatjana-chernjaeva <42931673+tatjana-chernjaeva@users.noreply.github.com>
2018-09-06 11:32:23 +0300

    Add files via upload

--- js/my_custom.js
+++ js/my_custom.js
@@ -10,6 +10,10 @@ function send_msg() {
 	frm = document.getElementById("IFrame1");
 
 	frm.contentWindow.document.getElementById('sender_name').value = 'Вася Пупкин';
+	frm.contentWindow.document.getElementById('sender_email').value = 'pup@mail.ru';
+	frm.contentWindow.document.getElementById('sender_phone').value = '+79161148255';
+	frm.contentWindow.document.getElementById('sender_topic').value = 'Привет Мир!';
+	frm.contentWindow.document.getElementById('sender_message').value = 'Добрый день всем привет!';
 	frm.contentWindow.document.getElementById('from_domen').value = 'Tanjana-Chernyaeva';
 	frm.contentWindow.document.getElementById("send_btn").click();
 	
